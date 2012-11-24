0073216df26c484d927b7378c5b9b18a9cf98d48
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2012-11-24 17:46:21 +0000

    First cut of some animation.

--- FireballRender.hs
+++ FireballRender.hs
@@ -7,6 +7,8 @@ import System.Environment
 import System.FilePath
 import System.IO
 import Trace.Hpc.Mix
+import Trace.Hpc.Util
+import Control.Monad
 
 main :: IO ()
 main = do
@@ -26,7 +28,7 @@ go name = do
   mixFiles <- mapM (readMix [".hpc", "."] . Left) moduleNames
 
   withFile (name <.> "html") WriteMode $ \html -> do
-    hPutStr html $ unlines
+    hPutStrLn html $ unlines
       [ " <!DOCTYPE HTML>"
       , " <html>"
       , "   <head>"
@@ -38,10 +40,12 @@ go name = do
       , "     <div class=\"header\">"
       , "       <h1>" ++ name ++ "</h1>"
       , "     </div>"
-      , " "
       ]
 
-    hPutStr html $ unlines
+    addTabBar html moduleNames
+    zipWithM_ (addSourceDiv html) moduleNames mixFiles
+
+    hPutStrLn html $ unlines
       [ "     <script src=\"jquery-1.8.3.js\"></script>"
       , "     <script src=\"" ++ (name <.> "js") ++ "\"></script>"
       , "     <script src=\"code.js\"></script>"
@@ -49,4 +53,63 @@ go name = do
       , " </html>"
       ]
 
-  error "TODO"
+  withFile (name <.> "js") WriteMode $ \js -> do
+    hPutStr js $ "ticks = ["
+
+    hPutStr js $ concat . intersperse ", " . flip map tixs $ \(name, tvs) ->
+      "{ name: " ++ show name ++ ", tixs: " ++
+        show (map (fromEnum . uncurry (/=)) tvs) ++ "}"
+
+    hPutStr js "]"
+    hPutStrLn js ""
+
+
+addTabBar :: Handle -> [String] -> IO ()
+addTabBar html moduleNames = do
+  hPutStr html "<div id=\"source-tabs\">"
+
+  forM_ moduleNames $ \name -> do
+    hPutStrLn html $ unlines
+      [ "<div id=\"source-tab-" ++ name ++ "\" class=\"source-tab\">"
+      , name
+      , "</div>"
+      ]
+
+  hPutStr html "</div>"
+
+addSourceDiv :: Handle -> String -> Mix -> IO ()
+addSourceDiv html name (Mix fp ts hash tabsize entries) = do
+  sourceCode <- map (map Left) . lines <$> readFile fp
+  let sourceCode' = foldl' addEntry sourceCode (entries `zip` [0..])
+
+  hPutStrLn html $ "<div id=\"source-" ++ name ++ "\" class=\"source\">"
+
+  forM_ sourceCode' $ \(codeLine :: [Either Char String])-> do
+    hPutStrLn html $ concatMap (either return id) codeLine
+  hPutStrLn html "</div>"
+
+  where
+    addEntry :: [[Either Char String]] -> (MixEntry, Integer) -> [[Either Char String]]
+    addEntry code ((pos, label), id) =
+      let boxType = case label of
+                      ExpBox _      -> "source-pos-type-exp"
+                      TopLevelBox _ -> "source-pos-type-toplevel"
+                      LocalBox _    -> "source-pos-type-local"
+                      BinBox _ _    -> "source-pos-type-bin"
+          (l1, c1, l2, c2) = fromHpcPos pos
+       in insertAt False l1 c1 ("<span class=\"source-pos " ++ boxType ++ "\" id=\"source-pos-" ++ name ++ "-" ++ (show id) ++ "\">")
+        . insertAt True l2 c2 "</span>"
+        $ code
+
+    insertAt :: Bool -> Int -> Int -> String -> [[Either Char String]] -> [[Either Char String]]
+    insertAt b l c txt code =
+      let (before, after:afters) = splitAt (l - 1) code
+       in before ++ [concat . snd $ mapAccumL insertAtLine 1 after] ++ afters
+       where
+          insertAtLine :: Int -> Either Char String -> (Int, [Either Char String])
+          insertAtLine p x
+            | p > c = (p, [x])
+          insertAtLine p (Left x)
+            | p == c    = (p + 1, (if b then reverse else id) [Right txt, Left x])
+            | otherwise = (p + 1, [Left x])
+          insertAtLine p x = (p, [x])
--- code.js
+++ code.js
@@ -1,5 +1,26 @@
+var idx = 0;
+
 var initialize = function () {
-  // TODO start!
+  console.log(ticks.length);
+  render()
+  window.setInterval(function() { step(); }, 100);
+}
+
+function render() {
+  var cpos = ticks[idx];
+
+  cpos.name;
+  cpos.tixs;
+
+  for(var i = 0 ; i < cpos.tixs.length ; i++) {
+    var elem = $('#source-pos-'+ cpos.name + '-' + i);
+    elem.toggleClass("source-pos-active", cpos.tixs[i] == 1);
+  }
+}
+
+function step() {
+  idx = (idx + 1) % ticks.length;
+  render();
 }
 
 
