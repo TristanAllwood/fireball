95360222fe14ab64c8a3ccb0e15a830f86eaea6f
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2012-11-24 21:36:13 +0000

    Progress bar.

--- code.js
+++ code.js
@@ -1,16 +1,31 @@
 var idx = 0;
 
 var initialize = function () {
-  console.log(ticks.length);
-  render()
+  addStatusControls();
+  render();
   window.setInterval(function() { step(); }, 100);
 }
 
+function addStatusControls() {
+  $("#source-tabs").append('<div id="controls"></div>');
+
+  controls = $("#controls");
+
+  controls.append('<div id="progress"><div id="progress-marker"></div></div>');
+}
+
 function render() {
+  var previousPos = ticks[ idx == 0 ? 0 : idx - 1]
   var cpos = ticks[idx];
 
-  cpos.name;
-  cpos.tixs;
+  if (previousPos.name != cpos.name) {
+    $(".source").toggle(false);
+    $(".source-tab").toggleClass("source-tab-active", false);
+    $("#source-" + cpos.name).toggle(true);
+    $("#source-tab-" + cpos.name).toggleClass("source-tab-active",true);
+  }
+
+  $("#progress-marker").css('margin-left', (idx * 100 / ticks.length) + '%');
 
   for(var i = 0 ; i < cpos.tixs.length ; i++) {
     var elem = $('#source-pos-'+ cpos.name + '-' + i);
